819c687c0e129c5662bc1c08292eff7a4604d15a
Harrisonkamau <kamauharrison87@gmail.com>
2016-11-27 10:09:06 +0300

    create route for reporting violence

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,17 @@
+var path = require('path');
+
 var routes = function(app){
   app.route('/')
     .get(function(req, res){
       res.sendFile(__dirname+ "/index.html");
 
     })
+    app.route('/report')
+    .get(function(req, res){
+      res.sendFile('/home/harry/Documents/hackathons/GoGirl/public/reportviolence.html');
+
+    });
+    
 }
 
 module.exports = routes;
