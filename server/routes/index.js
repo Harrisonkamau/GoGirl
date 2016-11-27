aa806ab1e412f5c98ba5889c4d1d778de77cd089
Harrisonkamau <kamauharrison87@gmail.com>
2016-11-27 15:33:49 +0300

    modify report router

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,19 +1,14 @@
 var path = require('path');
 // var public = "/home/harry/Documents/hackathons/GoGirl/public";
 var routes = function(app){
-  // app.route('/')
-  //   .get(function(req, res){
-    
-  //     res.sendFile(__dirname+ "/index.html");
+  app.route('/')
+    .get(function(req, res){
+      res.sendFile("index.html", {root: path.join(__dirname, '../../public')})
 
-
-  //   }),
+    })
     app.route('/report')
     .get(function(req, res){
-      // res.sendFile('/home/harry/Documents/hackathons/GoGirl/public/reportviolence.html');
-
       res.sendFile("reportviolence.html", {root: path.join(__dirname, '../../public')});
-      // console.log(__dirname);
     });
     
 }
