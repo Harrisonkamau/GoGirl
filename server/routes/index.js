4ed8240e3497612404540c2a046a3f26c3f575b7
Harrisonkamau <kamauharrison87@gmail.com>
2016-11-27 13:59:48 +0300

    define report route

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,15 +1,19 @@
 var path = require('path');
-
+// var public = "/home/harry/Documents/hackathons/GoGirl/public";
 var routes = function(app){
-  app.route('/')
-    .get(function(req, res){
-      res.sendFile(__dirname+ "/index.html");
+  // app.route('/')
+  //   .get(function(req, res){
+    
+  //     res.sendFile(__dirname+ "/index.html");
 
-    })
+
+  //   }),
     app.route('/report')
     .get(function(req, res){
-      res.sendFile('/home/harry/Documents/hackathons/GoGirl/public/reportviolence.html');
+      // res.sendFile('/home/harry/Documents/hackathons/GoGirl/public/reportviolence.html');
 
+      res.sendFile("reportviolence.html", {root: path.join(__dirname, '../../public')});
+      // console.log(__dirname);
     });
     
 }
